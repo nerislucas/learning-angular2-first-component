df66f35a42e7878e85bf34d544a71e857da640c5
Lucas Neris <lucas.neris@globalpoints.com.br>
2018-03-17 09:25:58 -0300

    first and second component

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { FirstComponent } from './first-component/first-component';
+import { SecondComponentComponent } from './second-component/second-component.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FirstComponent,
+    SecondComponentComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/first-component/first-component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    // tslint:disable-next-line:component-selector
+    selector: 'app-first-component',
+    template: `<p>My first component in Angular 2!`
+})
+export class FirstComponent { }
